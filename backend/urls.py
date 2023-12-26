0d4abd8dfc57f6ee02a2bf80a4b1f81a98aefa72
Harsh Gor <harshmgor@gmail.com>
2023-12-26 20:48:22 +0530

    rest setup

--- backend/urls.py
+++ backend/urls.py
@@ -1,4 +1,4 @@
-"""django_rest_app URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
+router = DefaultRouter()
+
+urlpatterns = router.urls
+
+urlpatterns += [
     path("admin/", admin.site.urls),
 ]
